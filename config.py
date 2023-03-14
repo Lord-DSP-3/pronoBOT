4f4c92d3812a23df741bcbff3530e16dbb0175be
Rampage.heart <107468577+Lord-DSP-3@users.noreply.github.com>
2023-03-14 11:58:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -52,7 +52,7 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "⚠️𝗬𝗼𝘂 𝗻𝗲𝗲
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 #set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
 
 #Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
