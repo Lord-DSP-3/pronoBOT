e594affcb24e987ced56c8259b7056e44c5df862
Rampage.heart <107468577+Lord-DSP-3@users.noreply.github.com>
2023-03-14 20:01:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,31 +13,31 @@ from logging.handlers import RotatingFileHandler
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "15448265"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "e0d55dc4e8402afa6655bea786c9d952")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001849739491"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1302714537"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
+DB_NAME = os.environ.get("DATABASE_NAME", "pVbot3seg")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001909817407"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hey {first} 🍻\n**This bot will let access to all kind of awesome P🔞RN content for free.**\n⚠️__Since it's free service: you cannont save or forward content.__")
+START_MSG = os.environ.get("START_MESSAGE", "Hey {first} 🍻\n<b>This bot will let access to all kind of awesome P🔞RN content for free.</b>\n⚠️<i>Since it's free service: you cannont save or forward content.</i>\n\nNOW GO TO THE CHANNEL BELOW AND START BOT WITH THIER BUTTON")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "⚠️𝗬𝗼𝘂 𝗻𝗲𝗲𝗱 𝘁𝗼 𝗷𝗼𝗶𝗻 𝗶𝗻 𝗺𝘆 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 𝘁𝗼 𝘂𝘀𝗲 𝗧𝗵𝗶𝘀 𝗯𝗼𝘁!\n👉🏻 @FREE_ADULT_VIDEO\n👉🏻 @pervertcollection \n__Join Channel or fuck off__")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "⚠️𝗬𝗼𝘂 𝗻𝗲𝗲𝗱 𝘁𝗼 𝗷𝗼𝗶𝗻 𝗶𝗻 𝗺𝘆 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 𝘁𝗼 𝘂𝘀𝗲 𝗧𝗵𝗶𝘀 𝗯𝗼𝘁!\n👉🏻 @FREE_ADULT_VIDEO\n👉🏻 @pervertcollection")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
