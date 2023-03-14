0ff19dd1c3c63f4c58c90be6008b1c18d404046a
Rampage.heart <107468577+Lord-DSP-3@users.noreply.github.com>
2023-03-14 20:12:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -155,12 +155,6 @@ async def not_joined(client: Client, message: Message):
         quote = True,
     )
 
-@Bot.on_message(filters.command('users') & filters.chat(AG) & filters.user(ADMINS))
-async def get_users(client: Bot, message: Message):
-    msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
-    users = await full_userbase()
-    await msg.edit(f"{len(users)} users are using this bot")
-
 @Bot.on_message(filters.chat(AG) & filters.command('broadcast') & filters.user(ADMINS))
 async def send_text(client: Bot, message: Message):
     if message.reply_to_message:
